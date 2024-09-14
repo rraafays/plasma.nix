1b1a64f6d213e404f9f941cd06860e4d6117a731
raf <rraf@tuta.io>
2024-09-14 09:22:35 +0100

    chore: created basic plasma module

--- default.nix
+++ default.nix
@@ -1,5 +1,46 @@
-{ ... }:
+{ pkgs, ... }:
 
+let
+  USER = "raf";
+
+  plasma = {
+    command = "${pkgs.plasma-workspace}/bin/startplasma-wayland";
+    user = USER;
+  };
+in
 {
+  services = {
+    xserver.enable = true;
+    desktopManager.plasma6.enable = true;
+  };
 
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = plasma;
+      default_session = plasma;
+    };
+  };
+
+  home-manager.users.${USER} = {
+    systemd.user.services.mouseless = {
+      Unit = {
+        Description = "mouseless";
+      };
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+      Service = {
+        ExecStart = "${pkgs.writeShellScript "mouseless" ''
+          #!/run/current-system/sw/bin/bash
+          ${pkgs.nur.repos.wolfangaukang.mouseless}/bin/mouseless --config ~/.config/mouseless/config.yaml
+        ''}";
+      };
+    };
+
+    home.packages = with pkgs; [
+      nur.repos.wolfangaukang.mouseless
+      kitty
+    ];
+  };
 }
