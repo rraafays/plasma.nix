16d351b9672c2389219d12c998b2b60e20d93b6b
raf <rraf@tuta.io>
2024-10-19 20:04:52 +0100

    update: using sddm

--- default.nix
+++ default.nix
@@ -2,45 +2,25 @@
 
 let
   USER = "raf";
-
-  plasma = {
-    command = "${pkgs.plasma-workspace}/bin/startplasma-wayland";
-    user = USER;
-  };
 in
 {
   services = {
     xserver.enable = true;
     desktopManager.plasma6.enable = true;
+    displayManager.enable = true;
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
   };
 
-  services.greetd = {
-    enable = true;
-    settings = {
-      initial_session = plasma;
-      default_session = plasma;
-    };
-  };
+  programs.dconf.enable = true;
 
   home-manager.users.${USER} = {
-    systemd.user.services.mouseless = {
-      Unit = {
-        Description = "mouseless";
-      };
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
-      Service = {
-        ExecStart = "${pkgs.writeShellScript "mouseless" ''
-          #!/run/current-system/sw/bin/bash
-          ${pkgs.nur.repos.wolfangaukang.mouseless}/bin/mouseless --config ~/.config/mouseless/config.yaml
-        ''}";
-      };
-    };
-
     home.packages = with pkgs; [
       nur.repos.wolfangaukang.mouseless
       kitty
+      konsave
+      kde-gtk-config
+      unstable.polonium
     ];
   };
 }
